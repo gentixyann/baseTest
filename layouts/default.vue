7e15fcb4752263919bdafa7be7449e60e635258c
Gento <a551.return@gmail.com>
2021-09-02 22:02:03 +0900

    first

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+<v-container>
+   <v-row>
+    <v-col class="text-center">
+      <img
+        src="/img/start.JPG"
+        class="mb-5 topImg"
+      >
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col class="text-center">
+     <v-btn rounded to="/inspire">診断スタート</v-btn>
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+
+<style scoped>
+.topImg {
+  width: 100px;
+  height: 100px;
+}
+</style>
+
