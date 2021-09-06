33756400804d2647b24b8b4f90604cbef4467c3c
Gento <a551.return@gmail.com>
2021-09-06 21:46:02 +0900

    質問内容をまとめたdata.jsを追加

---
+++ assets/data.js
@@ -0,0 +1,38 @@
+export const answersData = [
+     {
+      id: 1,
+      title: "質問１",
+      questions: [
+        {
+          id: 1,
+          questionText: "欲しいものは遠慮しすぎずに確実に手に入れる。",
+        },
+        {
+          id: 2,
+          questionText: "自分と周囲とのバランスを大切にする。",
+        },
+        {
+          id: 3,
+          questionText: "つい自分の中の世界にひたりがちになる。",
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "質問２",
+      questions: [
+         {
+          id: 1,
+          questionText: "楽観的すぎて問題になることがある。",
+        },
+        {
+          id: 2,
+          questionText: "冷静沈着で合理的な人と思われる。",
+        },
+        {
+          id: 3,
+          questionText: "気持ちを発散したら、気がおさまる。",
+        }
+      ]
+    }
+]
