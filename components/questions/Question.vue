ce32e5b2a6b8653cbc0a74977fb637e65a868928
lnoueryo <popo62520908@gmail.com>
2021-10-01 22:38:18 +0900

    transition追加

--- components/questions/Question.vue
+++ components/questions/Question.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="mt-5">
-    <!--質問表示-->
-    <h2>{{ question.title }}</h2>
-    <img
-      :src="require(`@/assets/images/questions/${question.img}`)"
-      class="mb-4 contentImg"
-    />
-    <div>
-      <!--選択肢answersをfor文で回す-->
-      <p v-for="(answer, i) in question.answers" :key="i">
-        <!--回答の選択肢を表示-->
-        {{ initial[i] }}. {{ answer.text }}
-      </p>
+  <transition name="fade">
+    <div class="mt-5" v-if="ready">
+      <!--質問表示-->
+      <h2>{{ question.title }}</h2>
+      <img
+        :src="require(`@/assets/images/questions/${question.img}`)"
+        class="mb-4 contentImg"
+      />
+      <div>
+        <!--選択肢answersをfor文で回す-->
+        <p v-for="(answer, i) in question.answers" :key="i">
+          <!--回答の選択肢を表示-->
+          {{ initial[i] }}. {{ answer.text }}
+        </p>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -22,6 +24,11 @@ export default Vue.extend({
   // index.vueから受け取ったquestionをpropsで受け取る
   // questionはdata.jsの問題１つ分
   props: ["question"],
+  data() {
+    return {
+      ready: false,
+    }
+  },
   computed: {
     //選択肢の数だけinitialにA,B,Cと自動でつく
     initial() {
@@ -30,5 +37,14 @@ export default Vue.extend({
       );
     },
   },
+  watch: {
+    question: {
+      handler() {
+        this.ready = false;
+        setTimeout(() => this.ready = true, 10)
+      },
+      immediate: true
+    },
+  }
 });
 </script>
